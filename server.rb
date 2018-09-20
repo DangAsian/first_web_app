c5ed7770697802296e258ceae45b1f532e3adc28
Daniel Ang <dan.justin.ang@gmail.com>
2018-09-20 19:32:40 -0400

    finished

--- server.rb
+++ server.rb
@@ -1,8 +1,30 @@
     require 'sinatra'
-    get '/home' do
-    erb :index
-  end
+
+get '/home' do
+  erb :index
+end
 
 
 
-    
+get '/portfolio' do
+  erb :gallery
+
+end
+
+get '/about_me' do
+  @skills = ['git', 'HTML', 'CSS', 'Ruby']
+  @interests = ['hedgehogs','games']
+  erb :about_me
+
+end
+
+
+get '/favourites' do
+  @fav_links = ["www.google.com", "www.tradingview.com"]
+  erb :fav_link
+end
+
+
+get '/' do
+  redirect to ('/home')
+end
